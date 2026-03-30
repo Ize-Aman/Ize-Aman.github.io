a34b104e76be9ae4ac3b91b81d522c499c4e5e7a
Aman <dmsammanuel@gmail.com>
2026-03-30 23:19:04 +0300

    projects page finished

--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -1,16 +1,11 @@
 import Pdf from "@/components/pdf";
 import React from "react";
-import { pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
 
 const Resume = () => {
     return (
         <div>
-            <Pdf />
+            <h1>Resume</h1>
         </div>
     )
 };
